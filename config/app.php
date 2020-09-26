1fe624756628776ee71c8f5d0493fff6c178282b
Georgy <george.ouma@strathmore.edu>
2020-09-26 18:34:07 +0300

    final

--- config/app.php
+++ config/app.php
@@ -162,8 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Unisharp\Ckeditor\ServiceProvider::class,
-        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -230,7 +229,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
+
     ],
 
 ];
